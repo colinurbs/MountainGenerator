54ee63e3897dbbee2b8cdbc6b42ffaa019571738
Colin Urban <colin.urban@gmail.com>
2022-01-15 13:46:45 -0500

    color changing and a new secret mode

--- mountain.js
+++ mountain.js
@@ -6,6 +6,51 @@ $(document).ready(function(){
 		makeMountains('mountain3');
 	});
 
+	$(document).on({
+	    mouseenter: function() {
+	        $("#party").fadeIn();
+	    },
+	    mouseleave: function() {
+	        $("#party").fadeOut();
+	    }
+	}, "#hidden");
+
+	$('#party').click(function(){
+		window.intervalID = window.setInterval(doParty, 900);
+	});
+
+	$('#new-colors').click(function(){
+		newColors();
+	});
+
+	function doParty() {
+	 	makeMountains('mountain1');
+		makeMountains('mountain2');
+		makeMountains('mountain3');
+		newColors();
+	}
+
+	function newColors() {
+		var foreground = '#'+Math.floor(Math.random()*16777215).toString(16);
+		var midground = getComplementaryColor(foreground);
+		var background = getComplementaryColor(midground);
+		//console.log(foreground + " " + midground + " " + background);
+		$('#mountain1').css('background-color', foreground);
+		$('#mountain2').css('background-color', midground);
+		$('#mountain3').css('background-color', background);
+	}
+
+	function getComplementaryColor(color) {
+	    var c = color.slice(1),
+	        i = parseInt(c, 16),
+	        v = ((1 << 4 * c.length) - 1 - i).toString(16);
+
+	    while (v.length < c.length) {
+	        v = '0' + v;
+	    }
+	    return '#' + v;
+	}
+
 	function makeMountains(id){
 
 		var nodes = $('#nodes').val();
@@ -39,7 +84,6 @@ $(document).ready(function(){
 			}
 
 			return Math.floor(value);
-
 		}
 	}
 });
